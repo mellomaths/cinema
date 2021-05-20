ad49419f666dd52239fb0311c0c351dd8d972a29
Matheus Lima <matheus.lima@wexinc.com>
2021-05-20 14:26:41 -0300

    Add ClientsModule and ConfigModule

--- films-ms/src/app.module.ts
+++ films-ms/src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'films-ms',
+            brokers: process.env.KAFKA_BROKERS.split(','),
+          },
+          consumer: {
+            groupId: 'films-ms-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
