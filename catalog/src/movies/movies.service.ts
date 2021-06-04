4c1fe86afe3f671aa4c8b75897ff45b3abe4db9e
Matheus Lima <matheus.lima@wexinc.com>
2021-06-04 12:22:21 -0300

    Send message with created movie to kafka topic

--- catalog/src/movies/movies.service.ts
+++ catalog/src/movies/movies.service.ts
@@ -6,6 +6,8 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import { KafkaMessageDTO } from 'src/infrastructure/kafka/dto/kafka-message.dto';
+import { MovieCreateDTO } from './dto/movie-create.dto';
 
 @Injectable()
 export class MoviesService implements OnModuleInit, OnModuleDestroy {
@@ -21,5 +23,12 @@ export class MoviesService implements OnModuleInit, OnModuleDestroy {
 
   async onModuleDestroy() {
     await this.clientKafka.close().catch((e) => this.logger.error(e));
+  }
+
+  registerNewMovie(movieToRegister: MovieCreateDTO, requestId: string) {
+    const message = new KafkaMessageDTO();
+    message.requestId = requestId;
+    message.body = movieToRegister;
+    this.clientKafka.send('NewMovieRegistered', message).subscribe();
   }
 }
