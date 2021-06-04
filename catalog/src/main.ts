93f25e7f0d727d889249280b7fec730939a25f07
Matheus Lima <matheus.lima@wexinc.com>
2021-06-04 17:16:31 -0300

    Add Swagger endpoint builder

--- catalog/src/main.ts
+++ catalog/src/main.ts
@@ -1,10 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('MainModule');
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Catalog')
+    .setDescription(
+      'This service provides operations to register Movies, TV Shows, Animes into the Cinema catalog.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}.`);
 }
 bootstrap();
