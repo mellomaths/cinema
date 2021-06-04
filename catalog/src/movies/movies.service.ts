e12719d746bfc0d02c516cd748dbdda2bb65c033
Matheus Lima <matheus.lima@wexinc.com>
2021-06-04 11:50:18 -0300

    Add kafka service injecting into movies service

--- catalog/src/movies/movies.service.ts
+++ catalog/src/movies/movies.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger('MoviesService');
+  public constructor(
+    @Inject('KAFKA_SERVICE') private readonly clientKafka: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    this.clientKafka.subscribeToResponseOf('NewMovieRegistered');
+    await this.clientKafka.connect();
+  }
+
+  async onModuleDestroy() {
+    await this.clientKafka.close().catch((e) => this.logger.error(e));
+  }
+}
