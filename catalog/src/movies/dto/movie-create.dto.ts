0b44157c8040a280d099c18a1d76171240397a86
Matheus Lima <matheus.lima@wexinc.com>
2021-06-04 15:57:38 -0300

    Change validation type to number

--- catalog/src/movies/dto/movie-create.dto.ts
+++ catalog/src/movies/dto/movie-create.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDecimal, IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class MovieCreateDTO {
   @IsNotEmpty()
@@ -31,7 +31,7 @@ export class MovieCreateDTO {
   @ApiProperty()
   year: number;
 
-  @IsDecimal()
+  @IsNumber()
   @ApiProperty()
   rating: number;
 }
