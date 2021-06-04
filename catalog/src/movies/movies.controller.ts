0a5bd46e70437eb04effa4446c3d992977cc9814
Matheus Lima <matheus.lima@wexinc.com>
2021-06-04 11:49:55 -0300

    Add basic structure for a Movies controller

--- catalog/src/movies/movies.controller.ts
+++ catalog/src/movies/movies.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  HttpStatus,
+  Logger,
+  Post,
+} from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { MovieCreateDTO } from './dto/movie-create.dto';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  private readonly logger = new Logger('MoviesController');
+
+  @Post()
+  @ApiOperation({ summary: 'Register a new movie in the Cinema.' })
+  @ApiBody({ type: MovieCreateDTO })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Movie successfully created.',
+    type: MovieCreateDTO,
+  })
+  registerNewMovie(
+    @Body() movieCreatedDto: MovieCreateDTO,
+    @Headers('request-id') requestId: string,
+  ) {
+    this.logger.log('Received payload: ' + JSON.stringify(movieCreatedDto));
+    this.logger.log(`Request ID: ${requestId}`);
+    return { ok: true };
+  }
+}
