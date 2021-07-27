0afae6f2648e15e587f40121adf6f1553b360dde
Matheus Lima <matheus.lima@wexinc.com>
2021-07-27 17:23:27 -0300

    Registering Kafka module in emails service

--- emails/src/emails/emails.module.ts
+++ emails/src/emails/emails.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { KafkaModule } from 'src/infrastructure/kafka.module';
 import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
 
 @Module({
+  imports: [KafkaModule.register()],
   controllers: [EmailsController],
   providers: [EmailsService],
 })
